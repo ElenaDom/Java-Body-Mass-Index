1fbddec2a1d463946c63f61f01977a59c0f25e79
Elena Domnina <bal.atidom@gmail.com>
2020-11-05 19:31:20 +0400

    Second

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public double calculate(double weight) {
-        double growth = 171.0*2/100;
+    public double calculate(double weight, double growth) {
+
         double index = weight/growth;
         return index;
 
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
         double weight = 58;
-        double index = service.calculate(weight);
+        double growth = 171.0*2/100;
+        double index = service.calculate(weight, growth);
         System.out.println(index);
 
 
